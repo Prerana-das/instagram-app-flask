37cf927a034d01ba003f2c3e4adf376f73f49c5a
Prerana <preranadas97@gmail.com>
2025-05-01 20:57:51 +0100

    updated

--- startup.sh
+++ startup.sh
@@ -1,1 +1,10 @@
-gunicorn --bind=0.0.0.0:8000 --workers=4 app:app
+#!/bin/bash
+
+# Build Vue app
+cd client
+npm install
+npm run build
+cd ..
+
+# Start Flask
+gunicorn --bind=0.0.0.0:8000 --timeout 120 --workers=4 app:app
