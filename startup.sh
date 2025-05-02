524a1a091406bc8abc6f25314b0d5a17771cb341
Prerana <preranadas97@gmail.com>
2025-05-02 03:19:22 +0100

    updated sh

--- startup.sh
+++ startup.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-# Navigate to client and build
-echo "Building starttt Vue..."
-cd client
-npm install
-npm run build
+cd client && npm install && npm run build
 cd ..
-
-
-echo "Building enddd Vue..."
-
-# Start Flask
-gunicorn --bind=0.0.0.0:8000 app:app
+gunicorn --bind=0.0.0.0:8000 --pythonpath=. app:app
