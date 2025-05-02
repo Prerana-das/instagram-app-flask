2ffd87fa0933071450296bcc8da3cc0060548747
Prerana <preranadas97@gmail.com>
2025-05-02 03:25:12 +0100

    updated sh nodejs

--- startup.sh
+++ startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Install Node.js
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 cd client && npm install && npm run build
 cd ..
 gunicorn --bind=0.0.0.0:8000 --pythonpath=. app:app
