403b20b2249612ecaac232deef7e1cb7376ba8c8
Prerana <preranadas97@gmail.com>
2025-05-02 03:30:57 +0100

    updated sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # Install Node.js
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
+export NVM_DIR="/usr/local/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Use appropriate Node.js version
+nvm install 18
+nvm use 18
 
 cd client && npm install && npm run build
 cd ..
